8481ed7cf0affc4501dc7d369ef4d2745d3cfb4d
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-02 18:23:15 -0400

    Create a sessions controller. Add create method and private methods for auth_hash and user params

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_or_create_by(email: user_params[:email])
+    user.update(user_params)
+
+    session[:user_id] = user.id
+
+    redirect_to root_path
+  end
+
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
+  def user_params
+    {google_id: auth_hash['uid'],
+      email: auth_hash['info']['email'],
+      token: auth_hash['credentials']['token']
+    }
+  end
+end
