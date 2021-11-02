42a4613b2436443b25d29859f801b598fd722277
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-02 18:21:10 -0400

    Installed yarn, webpacker, react

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,8 @@
 //= require react_ujs
 //= require components
 //= require_tree .
+
+// Support component names relative to this directory:
+var componentRequireContext = require.context("../components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
