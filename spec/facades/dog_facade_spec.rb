f09e106e2e40469987b14f8d053c204f15b11100
Tanner Dale <tanner@tannerdale.com>
2021-11-03 17:14:20 -0500

    Add method tests, method to get all dogs

---
+++ spec/facades/dog_facade_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe DogFacade, :vcr do
+  let(:params) do
+    {
+      name: 'a',
+      breed: 'b',
+      age: 1,
+      size: 1,
+      vaccinated: 1,
+      trained: 1,
+      user_id: 3
+    }
+  end
+
+  it 'forwards the request to the client' do
+    json = DogSerializer.new(params).to_json
+    result = DogFacade.create_dog(json)
+
+    expect(result).to be_empty
+  end
+
+  it 'can get an array of dog poros' do
+    result = DogFacade.dogs
+    all_poros = result.all? { |i| i.is_a? DogPoro }
+
+    expect(result).to be_an Array
+    expect(all_poros).to be true
+  end
+end
