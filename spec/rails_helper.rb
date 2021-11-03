d5a6501351f40491e0eea7070e531087f18cdcf1
Tanner Dale <tanner@tannerdale.com>
2021-11-02 21:34:16 -0500

    Through lonely operator in to avoid nil error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,9 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+SimpleCov.start
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
