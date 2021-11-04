cd3039b7ae24926f9ef2747e5101263a5cd5eab4
Tanner Dale <tanner@tannerdale.com>
2021-11-04 10:08:52 -0500

    Add method to ensure current user before visiting the dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def validate_user
+    permission_denied unless current_user
+  end
+
+  def permission_denied
+    redirect_to root_path, alert: 'Please login or sign up.'
+  end
 end
