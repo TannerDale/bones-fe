a967445d2dce8c3d3a0d8f8b4db67f14e9fcc0ef
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-05 12:27:45 -0400

    Add create method for playdate controller. Add cookies to store data

--- app/controllers/play_dates_controller.rb
+++ app/controllers/play_dates_controller.rb
@@ -1,8 +1,24 @@
 class PlayDatesController < ApplicationController
   def new
-    session[:playdate_dog_id] = params[:dog_id]
+    @user_dogs = DogFacade.user_dogs(current_user.id)
+    @location = params[:location_id]
   end
 
   def create
+    json = PlayDateSerializer.new(playdate_params.merge({ invited_dog_id: cookies[:invited_dog] }))
+    PlayDateFacade.create_play_date(json)
+
+    clear_invited_dog
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def playdate_params
+    params.permit(:date, :time, :creator_dog_id, :location_id)
+  end
+
+  def clear_invited_dog
+    cookies.delete(:invited_dog)
   end
 end
