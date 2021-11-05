73789339d24d462b086448bb5685874623ea8317
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-05 12:21:43 -0400

    Create a spec file for playdate new

--- spec/features/playdates/new_spec.rb
+++ spec/features/playdates/new_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'playdates#new' do
-  
+  let!(:locations) { build_list :location_poro, 20 }
+  let(:user) { create :user }
+  let(:host_dog) { create :dog }
+  let(:invited_dog) { create :dog }
+
+  before :each do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit new_play_date_path
+  end
+
+  it 'has a form to create a new playdate' do
+    expect(page).to have_content("New Playdate!")
+    expect(page).to have_field(:date)
+    expect(page).to have_field(:time)
+
+    fill_in :date, with: "2021/10/20"
+    fill_in :time, with: "05:00 AM"
+
+    click_button "Create Playdate"
+
+    expect(current_path).to eq(dashboard_path)
+  end
 end
