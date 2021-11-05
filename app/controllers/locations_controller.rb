65eafbed8282cbe491043b47abf256bc7a7ee901
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-05 12:27:55 -0400

    Create index method in locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,13 @@
 class LocationsController < ApplicationController
-
   def index
-    @locations = LocationPoro.all
+    @page = params[:page]&.to_i || 1
+    @locations = LocationFacade.nearby_locations(location)
+    cookies[:invited_dog] = params[:dog_id]
+    # @locations = LocationSerializer.new(locations)
+    #
+    # respond_to do |f|
+    #   f.html
+    #   f.json { render json: { locations: @locations } }
+    # end
   end
 end
