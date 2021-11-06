2d1f3316383c88277c5ad6bee451619152a964df
Christina Delpone <cdelpone@gmail.com>
2021-11-05 20:43:35 -0600

    test edge case for nonexistant invited dog

--- spec/features/playdates/new_spec.rb
+++ spec/features/playdates/new_spec.rb
@@ -3,17 +3,16 @@ require 'rails_helper'
 RSpec.describe 'playdates#new', :vcr do
   let!(:locations) { build_list :location_poro, 20 }
   let(:user) { create :user }
-  let(:host_dog) { build :dog_poro }
-  let(:invited_dog) { build :dog_poro }
+  let(:host_dog) { build :dog_poro, id: 1 }
 
   before :each do
     allow(DogFacade).to receive(:user_dogs).and_return([host_dog])
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    create_cookie(:invited_dog, 4)
+    create_cookie(:invited_dog, 2)
 
-    visit new_play_date_path
+    visit new_play_date_path(location_id: 'sadfgjh')
   end
 
   it 'has a form to create a new playdate' do
@@ -51,6 +50,19 @@ RSpec.describe 'playdates#new', :vcr do
 
   it 'requires date field to be filled in' do
     fill_in :time, with: "05:00 AM"
+    select host_dog.name
+
+    click_button "Create Playdate"
+
+    expect(current_path).to eq(new_play_date_path)
+  end
+
+  it 'dog doesnt exist, it renders new' do
+    #in case a dev edits the cookies
+    fill_in :date, with: Date.today + 1
+    fill_in :time, with: "05:00 AM"
+    create_cookie(:invited_dog, 100000000)
+
     select host_dog.name
 
     click_button "Create Playdate"
