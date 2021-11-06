42b5d520fe7f9b067af643721b9cf2d5309a0456
Christina Delpone <cdelpone@gmail.com>
2021-11-05 18:40:03 -0600

    Update location params

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,7 @@
 class LocationsController < ApplicationController
   def index
     @page = params[:page]&.to_i || 1
-    @locations = LocationFacade.nearby_locations(location)
+    @locations = LocationFacade.nearby_locations(params[:location])
     cookies[:invited_dog] = params[:dog_id]
     # @locations = LocationSerializer.new(locations)
     #
