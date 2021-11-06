d0b137dddca02080f7360d34597f0f13798b1368
Christina Delpone <cdelpone@gmail.com>
2021-11-05 20:42:19 -0600

    test playdate facade doesnt send back any info

---
+++ spec/facades/play_date_facade_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe PlayDateFacade, :vcr do
+  let(:location) { build :location_poro }
+  let(:invited_dog) { build :dog_poro }
+  let(:host_dog) { build :dog_poro }
+  let(:params) do
+    {
+      date: Date.today,
+      time: "05:00 AM",
+      creator_dog_id: 1,
+      location_id: location.id,
+      invited_dog_id: 2
+    }
+  end
+
+  it 'forwards the request to the client' do
+    json = PlayDateSerializer.new(params).to_json
+    result = PlayDateFacade.create_play_date(json)
+
+    expect(result).to be_empty
+    expect(result).to be_a String
+  end
+end
