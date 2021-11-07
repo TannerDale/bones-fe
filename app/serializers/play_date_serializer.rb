90dc753aa38f872dc3ede1039919f3241b8f1a7e
Jessica Jones <daddehjones@gmail.com>
2021-11-07 13:59:36 -0500

    refactor serializer without gem

--- app/serializers/play_date_serializer.rb
+++ app/serializers/play_date_serializer.rb
@@ -1,5 +1,17 @@
 class PlayDateSerializer
-  include JSONAPI::Serializer
-
-  attributes :date, :time, :creator_dog_id, :location_id, :invited_dog_id
+  class << self
+    def serialize(data)
+      {
+        object: {
+          date: data[:date],
+          time: data[:time],
+          creator_dog_id: data[:creator_dog_id],
+          location_id: data[:location_id],
+          invited_dog_id: data[:invited_dog_id]
+        },
+        token: data[:token],
+        email: data[:email]
+      }.to_json
+    end
+  end
 end
