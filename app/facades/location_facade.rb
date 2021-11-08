d53da05c223ef9703c7059f4dc37cfe987e17c42
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-08 15:56:27 -0500

    Refactor location facade and controller to take in an argument for location

--- app/facades/location_facade.rb
+++ app/facades/location_facade.rb
@@ -1,6 +1,6 @@
 class LocationFacade
   class << self
-    def nearby_locations(location = 'Denver')
+    def nearby_locations(location)
       LocationService.nearby_locations(location).map do |local|
         LocationPoro.new(local)
       end
