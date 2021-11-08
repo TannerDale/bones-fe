936e95880ffb606236c6f16669da3556e64dc9d3
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-08 15:33:58 -0500

    Add edit and update methods on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   before_action :validate_user
 
+  def edit; end
+
   def show
     @dogs = DogFacade.user_dogs(current_user.id)
+  end
+
+  def update
+    current_user.update(location: params[:location])
+    redirect_to dashboard_path
   end
 end
