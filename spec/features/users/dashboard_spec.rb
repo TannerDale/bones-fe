a832df956c4580d7854f759708264e355b69e30e
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-08 18:52:35 -0500

    Update tests for dashboard spec

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -3,17 +3,18 @@ require 'rails_helper'
 describe 'Dashboard Index', :vcr do
   let(:user) { create :user }
   let!(:dogs) { build_list :dog_poro, 3, user_id: user.id }
-
+  let(:dog1) { build :dog_with_play_dates_poro }
 
   before :each do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     allow(DogFacade).to receive(:user_dogs).and_return(dogs)
+
     visit dashboard_path
   end
 
   describe 'Profile information' do
     it 'has a link to edit user profile' do
-      within "#location" do
+      within '#location' do
         expect(page).to have_link('Edit Profile')
         click_link('Edit Profile')
       end
@@ -33,10 +34,10 @@ describe 'Dashboard Index', :vcr do
   describe 'Displays users dogs' do
     it 'has a link to each dogs show page' do
       dog = dogs.first
-      expect(page).to have_link("#{dog.name}")
-      click_link("#{dog.name}")
+      expect(page).to have_link(dog.name)
+      click_link(dog.name)
 
-      expect(current_path).to eq(dog_path(dog))
+      expect(current_path).to eq(dog_path(dog.id))
     end
   end
 
