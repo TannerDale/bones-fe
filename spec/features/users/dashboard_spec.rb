5ee31e405500f0c821d176476407e3d227ed5d2c
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-08 15:55:52 -0500

    Write test that verifies each dog has a link to its show page

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 describe 'Dashboard Index', :vcr do
   let(:user) { create :user }
+  let!(:dogs) { build_list :dog_poro, 3, user_id: user.id }
+
 
   before :each do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
+    allow(DogFacade).to receive(:user_dogs).and_return(dogs)
     visit dashboard_path
   end
 
@@ -23,7 +25,21 @@ describe 'Dashboard Index', :vcr do
   describe 'Create dog' do
     it 'has a link to create a dog' do
       expect(page).to have_link('Add new dog')
+      click_link('Add new dog')
       expect(current_path).to eq(new_dog_path)
     end
+  end
+
+  describe 'Displays users dogs' do
+    it 'has a link to each dogs show page' do
+      dog = dogs.first
+      expect(page).to have_link("#{dog.name}")
+      click_link("#{dog.name}")
+
+      expect(current_path).to eq(dog_path(dog))
+    end
+  end
+
+  describe 'Displays pending playdate invites' do
   end
 end
