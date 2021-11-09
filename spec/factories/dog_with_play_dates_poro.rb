71f92c11ac4aeefce4bb0ca37a0cfb350a3d5517
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-08 19:23:00 -0500

    Update factory

--- spec/factories/dog_with_play_dates_poro.rb
+++ spec/factories/dog_with_play_dates_poro.rb
@@ -11,8 +11,22 @@ FactoryBot.define do
           trained: %w[yes no].sample,
           sex: %w[male female].sample,
           user_id: (1..100).to_a.sample,
-          accepted_play_dates: (build_list :play_date_poro, 2),
-          pending_play_dates: (build_list :play_date_poro, 2)
+          accepted_play_dates: [{
+            invited_dog_id: 2,
+            creator_dog_id: 3,
+            date: Date.today + 1,
+            time: Time.new+ 1,
+            location_id: "asdasdad",
+            invite_status: 'Rejected'
+          }],
+          pending_play_dates: [{
+            invited_dog_id: 1,
+            creator_dog_id: 1,
+            date: (Date.today + 1),
+            time: (Time.new + 1),
+            location_id: "asdasdad",
+            invite_status: 'Nice'
+          }]
         }
       end
     initialize_with { new(attributes) }
