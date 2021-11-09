edfea91046aee87158e75865d239b71fbf3898e8
Jessica Jones <daddehjones@gmail.com>
2021-11-09 18:26:18 -0500

    Create find_for_user, Update_play_date and my_play_dates methods.

--- app/facades/play_date_facade.rb
+++ app/facades/play_date_facade.rb
@@ -3,5 +3,21 @@ class PlayDateFacade
     def create_play_date(json)
       PlayDateService.create_play_date(json)
     end
+
+    def find_for_user(id, status = 'all')
+      PlayDateService.find_for_user(id, status).map do |pd|
+        InvitedPlayDatePoro.new(pd)
+      end
+    end
+
+    def update_play_date(id, status)
+      PlayDateService.update_play_date(id, status)
+    end
+
+    def my_play_dates(id)
+      PlayDateService.my_play_dates(id).map do |play|
+        InvitedPlayDatePoro.new(play)
+      end
+    end
   end
 end
