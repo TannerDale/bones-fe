cb412319c9b17e1c594d168db3ea239f057e2bbc
Jessica Jones <daddehjones@gmail.com>
2021-11-09 18:23:57 -0500

    Update index route

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:edit, :update]
 
-  resources :play_dates, only: [:new, :create]
+  resources :play_dates, only: [:index, :new, :create, :update]
 
   delete 'logout', to: 'sessions#destroy'
 
