8497c3428641f83483ddbe8a425f6f1049caa03d
Christina Delpone <cdelpone@gmail.com>
2021-11-09 19:15:50 -0700

    add id to pd poro

--- app/poros/play_date_poro.rb
+++ app/poros/play_date_poro.rb
@@ -1,7 +1,8 @@
 class PlayDatePoro
-  attr_reader :invited_dog_id, :creator_dog_id, :date, :time, :location_id, :invite_status
+  attr_reader :id, :invited_dog_id, :creator_dog_id, :date, :time, :location_id, :invite_status
 
   def initialize(data)
+    @id = data[:id]
     @invited_dog_id = data[:invited_dog_id]
     @creator_dog_id = data[:creator_dog_id]
     @date = data[:date]
