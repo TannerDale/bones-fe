6498e66a249fecec27c90a43dfaadfa416a8131d
Christina Delpone <cdelpone@gmail.com>
2021-11-10 09:57:55 -0700

    test location facade returns location by id

--- spec/facades/location_facade_spec.rb
+++ spec/facades/location_facade_spec.rb
@@ -3,7 +3,16 @@ require 'rails_helper'
 describe LocationFacade, :vcr do
   it 'creates location poros for nearby locations' do
     result = LocationFacade.nearby_locations('Denver')
+    
     expect(result).to be_an Array
     expect(result.first).to be_a LocationPoro
+  end
+
+  it 'finds location by id' do
+    result = LocationFacade.search_by_id('MWFGHr6NOg7pAoLgbgFdsQ')
+
+    expect(result).to be_a LocationPoro
+    expect(result.id).to eq('MWFGHr6NOg7pAoLgbgFdsQ')
+    expect(result.name).to eq('Doppio Coffee')
   end
 end
